e6ff57b1e2ce82d2c3a070c8dd8cdba2dc90c162
Bruno-Paese <123875972+Bruno-Paese@users.noreply.github.com>
2024-05-19 22:17:13 -0300

    gerouuuuuuuuuuuuuuu

--- Terrain.gd
+++ Terrain.gd
@@ -8,9 +8,24 @@ extends StaticBody3D
 func generate_mesh_func():
 	print("generateMesh")
 	var plane_mesh = PlaneMesh.new()
-	plane_mesh.size = Vector2(4,4)
-	plane_mesh.subdivide_depth = 1000
-	plane_mesh.subdivide_width = 1000
+	plane_mesh.size = Vector2(100,100)
+	plane_mesh.subdivide_depth = 100
+	plane_mesh.subdivide_width = 100
 
-	$MeshInstance3D.mesh = plane_mesh
+	var surface_tool = SurfaceTool.new()
+	surface_tool.create_from(plane_mesh, 0)
+	var data = surface_tool.commit_to_arrays()
+	var vertices = data[ArrayMesh.ARRAY_VERTEX]
+	
+	var noise = FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+
+	for i in vertices.size():
+		vertices[i].y = noise.get_noise_2d(vertices[i].x, vertices[i].z) * 10
+	data[ArrayMesh.ARRAY_VERTEX] = vertices
+
+	var array_mesh = ArrayMesh.new()
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, data)
+
+	$MeshInstance3D.mesh = array_mesh
 	return true
